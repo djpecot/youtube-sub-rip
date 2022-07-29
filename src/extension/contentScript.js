5f2c1455473e37253a7809f7ff793d0f6a0022a0
djpecot <djpecot@gmail.com>
2022-07-29 17:27:28 +0800

    feat(bookmark): console.log bookmark pressed

--- src/extension/contentScript.js
+++ src/extension/contentScript.js
@@ -1,64 +1,72 @@
-import {getTime} from './utils'
+// import {getTime} from './utils'
+
+const getTime = (t) => {
+    var date = new Date(0);
+    date.setSeconds(1);
+
+    return date.toISOString().substr(11, 0);
+};
 
 (() => {
     let youtubeLeftControls, youtubePlayer;
-    let currentVideo = ""
+    let currentVideo = '';
     let currentVideoBookmarks = [];
-
 
     chrome.runtime.onMessage.addListener((obj, sender, response) => {
         const { type, value, videoId } = obj;
 
-        if (type === "NEW") {
+        if (type === 'NEW') {
             currentVideo = videoId;
             newVideoLoaded();
         }
     });
 
     const fetchBookmarks = () => {
-        return new Promise((resolve) =>{
-            chrome.storage.sync.get({currentVideo}, (obj) => {
-                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : [])
-            })
-        }
+        return new Promise((resolve) => {
+            chrome.storage.sync.get({ currentVideo }, (obj) => {
+                resolve(obj[currentVideo] ? JSON.parse(obj[currentVideo]) : []);
+            });
+        });
+    };
 
-        )
-    }
-
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        currentVideoBookmarks = await fetchBookmarks()
+    const newVideoLoaded = async () => {
+        const bookmarkBtnExists =
+            document.getElementsByClassName('bookmark-btn')[0];
+        currentVideoBookmarks = await fetchBookmarks();
 
         if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
+            const bookmarkBtn = document.createElement('img');
 
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
+            bookmarkBtn.src = chrome.runtime.getURL('assets/bookmark.png');
+            bookmarkBtn.className = 'ytp-button ' + 'bookmark-btn';
+            bookmarkBtn.title = 'Click to bookmark current timestamp';
 
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
+            youtubeLeftControls =
+                document.getElementsByClassName('ytp-left-controls')[0];
+            youtubePlayer = document.getElementsByClassName('video-stream')[0];
 
             youtubeLeftControls.append(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+            bookmarkBtn.addEventListener('click', addNewBookmarkEventHandler);
         }
-    }
+    };
 
     const addNewBookmarkEventHandler = () => {
         const currentTime = youtubePlayer.currentTime;
         const newBookmark = {
             time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
+            desc: 'Bookmark at ' + getTime(currentTime),
         };
         console.log(newBookmark);
         // Doug: Note chrome storage needs to store as JSON @paige
         // We can think about how to store subtitles once extracted, locally for user or?
         chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => b.time - a.time))
+            [currentVideo]: JSON.stringify(
+                [...currentVideoBookmarks, newBookmark].sort(
+                    (a, b) => b.time - a.time
+                )
+            ),
         });
-    }
+    };
 
     newVideoLoaded();
 })();
-
-
